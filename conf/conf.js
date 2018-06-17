19cea3c9d967843f9429088221cdaa79e6184ca6
LuckyHH <1391380241@qq.com>
2018-06-17 12:08:38 +0800

    update heroku

--- conf/conf.js
+++ conf/conf.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // HOSTNAME: "http://websplider.herokuapp.com",
-    HOSTNAME: "http://localhost:3000",
-    // DataBaseUrl: 'luckyhh:hj782161@ds147890.mlab.com:47890/websplider',
-    DataBaseUrl: "localhost:27017"
+    HOSTNAME: "http://websplider.herokuapp.com",
+    // HOSTNAME: "http://localhost:3000",
+    DataBaseUrl: 'luckyhh:hj782161@ds147890.mlab.com:47890/websplider',
+    // DataBaseUrl: "localhost:27017"
 }
