875b7ce881c9771479d42182267895b4af6dc116
luckyhh <gotoanything@foxmail.com>
2018-08-15 01:29:13 +0800

    fix proxy bug

--- conf/conf.js
+++ conf/conf.js
@@ -1,4 +1,4 @@
 module.exports = {
-    DataBaseUrl: "localhost:27017/websplider",
-    HOSTNAME: "http://localhost:3000"
+    HOSTNAME: "http://websplider.herokuapp.com",
+    DataBaseUrl: 'luckyhh:hj782161@ds147890.mlab.com:47890/websplider',
 }
